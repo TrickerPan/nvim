05714381315f8ec28659ec0c4c4c2e0d275efca6
Mr.Tricker <tricker.pan@gmail.com>
2025-08-14 18:14:27 +0800

    添加 null-ls 依赖并配置

--- lua/deps/editing.lua
+++ lua/deps/editing.lua
@@ -1,1 +1,16 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+later(function()
+    add({
+        source = 'nvimtools/none-ls.nvim',
+        checkout = 'main',
+        depends = {
+            {
+                source = 'nvimtools/none-ls-extras.nvim',
+                checkout = 'main',
+            }
+        },
+    })
+    local null_ls = require("null-ls")
+    null_ls.setup()
+end)
